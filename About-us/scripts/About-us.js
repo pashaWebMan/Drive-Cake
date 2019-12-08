3a32105499a65df7fd1f11a140c12ce0ca8c44a5
PlayPartyDev <pashaboiko55@gmail.com>
2019-12-08 10:47:11 +0200

    mobile lock screen

--- About-us/scripts/About-us.js
+++ About-us/scripts/About-us.js
@@ -8,6 +8,5 @@ function closeMobileNav() {
   mobileNav[0].style.top = "-60%"
 }
 
-if (window.screen.orientation.type == 'portrait-primary') {
-	window.screen.orientation.lock('portrait-primary')
-};
+let currentScreenOrientation = window.screen.orientation.type;
+window.screen.orientation.lock(currentScreenOrientation);
